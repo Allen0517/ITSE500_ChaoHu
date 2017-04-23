6012a1d027f2f5a016dbc9315ae24781b79b2c02
Allen <hclovejesus@gmail.com>
2017-04-23 16:47:02 -0300

    push lesson 1

---
+++ src/edu/oit/lesson1/src/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        MineInfor allen = new MineInfor();
+
+        System.out.println("Hello World!");
+        System.out.println(MineInfor.myName("Chao", "Hu"));
+        System.out.println(allen.birthYear(25));
+    }
+}
+
+//static method you need use the class name plus method name to get it; public method you need get them through instance
+
---
+++ src/edu/oit/lesson1/src/MineInfor.java
@@ -0,0 +1,13 @@
+/**
+ * Created by IT on 2017/4/23.
+ */
+public class MineInfor {
+
+    public static String myName(String firstname, String lastname) {
+        return lastname + " " + firstname;
+    }
+
+    public int birthYear(int year){
+        return year;
+    }
+}
